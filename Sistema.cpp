ff2f61f4505a877880c7c1080640ba5e9a68bffd
HaslinD <haslind@unitec.edu>
2017-06-20 19:39:01 -0600

    to Save

--- Sistema.cpp
+++ Sistema.cpp
@@ -23,6 +23,7 @@
 #include "Manzana.h"
 #include "Cono.h"
 #include "Sundae.h"
+#include <typeinfo>
 //#include "Productos.h"
 
 
@@ -432,6 +433,114 @@ void Sistema::run(){
           }//fin agregar
           else if(opcion[0]=='2'){//modificar
             cleanScreen();
+            bool val=false;
+            while(!val){
+              mvprintw(2, 10, "Que desea Modificar ");
+              mvprintw(3, 10, "[1]. Bebidas");
+              mvprintw(4, 10, "[2]. Carnes");
+              mvprintw(5, 10, "[3]. Frutas");
+              mvprintw(6, 10, "[4]. Heladeria");
+              mvprintw(7, 10, "[5]. Herramientas");
+              mvprintw(8, 10, "[6]. Jugueteria");
+              mvprintw(9, 10, "[7]. Licores");
+              mvprintw(10, 10, "[8]. Limpieza");
+              mvprintw(11, 10, "[9]. Mariscos");
+              mvprintw(12, 10, "[10]. Pan");
+              mvprintw(13, 10, "[11]. Verduras");
+              mvprintw(14, 10, "[12]. Salir");
+              mvprintw(15, 10, "ELIGA LA OPCION: ");
+              char respuesta[1];
+              getstr(respuesta);
+              string res = static_cast<char*>(respuesta);
+              getch();
+              if (res == "1") { //Mod Bebidas
+                for (int i = 0; i < productos.size(); ++i) {
+                cleanScreen();
+                bool libre=false;
+                while(!libre){
+                  mvprintw(5,20,"Cual desea Modificar: ");
+                  mvprintw(6,20,"[1]. Coca Cola");
+                  mvprintw(7,20,"[2]. Pepsi");
+                  mvprintw(8,20,"[3]. Salir");
+                  mvprintw(9,20,"Ingrese Opcion: ");
+                  char op[1];
+                  getstr(op);
+                  if (typeid(*productos[i]) == typeid(Bebidas)) {
+                    if (op[0]=='1'){//coca-cola
+                      cleanScreen();
+                      mvprintw(6,20,"Ingrese precio: ");
+                      char price[9];
+                      getstr(price);
+                      string precio = static_cast<char*>(price);
+                      mvprintw(7,20,"Ingrese codigo: ");
+                      char cod[9];
+                      getstr(cod);
+                      string codigo = static_cast<char*>(cod);
+                      mvprintw(8,20,"seleccione que Bebida Va Modificar: ");
+                      char Amod[9];
+                      getstr(Amod);
+                      string modify = static_cast<char*>(Amod);
+
+                      mvprintw(6,20,"Se ha agregado correctamente: ");
+                    }
+                    else if (op[0]=='2'){//pepsi
+                      cleanScreen();
+                      mvprintw(6,20,"Ingrese Precio: ");
+                      char price[9];
+                      getstr(price);
+                      string precio = static_cast<char*>(price);
+                      mvprintw(7,20,"Ingrese Código: ");
+                      char cod[9];
+                      getstr(cod);
+                      string codigo = static_cast<char*>(cod);
+                      Productos* bedi=new Pepsi(codigo,precio,"Pepsi");
+                      productos.push_back(bedi);
+                      mvprintw(6,20,"Se ha agregado correctamente: ");
+                    }
+                    else if (op[0]=='3'){//salir
+                      cleanScreen();
+                      libre=true;
+                    }else{
+                      cleanScreen();
+                      mvprintw(5,20,"Opcion no valida, intente de nuevo...");
+                      getch();
+                    }
+                    cleanScreen();
+                  }
+                }
+
+                getch();
+                }
+              } else if (res == "2") {//Mod Carnes
+                
+              } else if (res == "3") {//Mod Frutas
+                
+              } else if (res == "4") {//Mod Heladeria
+                
+              } else if (res == "5") {//Mod Herramientas
+                
+              } else if (res == "6") {//Mod Jugueteria
+                
+              } else if (res == "7") {//Mod Licores
+                
+              } else if (res == "8") {//Mod Limpieza
+                
+              } else if (res == "9") {//Mod Mariscos
+                
+              } else if (res == "10") {//Mod Pan
+                
+              } else if (res == "11") {//Mod Verduras
+                
+              } else if (res == "12") {
+                cleanScreen();
+                val=true;
+              }else{
+                cleanScreen();
+                mvprintw(5,20,"Opcion no valida, intente de nuevo...");
+                getch();
+              }
+              cleanScreen();
+            }
 
           }//fin Modificar
           else if (opcion[0]=='3'){//eliminar
